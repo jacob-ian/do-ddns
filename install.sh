c05fc7e60a9590ebd394a926c6a56d121f1bec4a
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-07-07 14:53:52 +1000

    Update install script

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@ cp ./target/release/do-ddns /usr/local/bin/do-ddns
 if [ ! -d "/etc/do-ddns" ]; then
   mkdir /etc/do-ddns
   echo "# Create the schedule on which to update the DNS record
-  # sec   min   hour   day of month   month   day of week   year
-  # *     *     *      *              *       *             *
-  schedule = \"0 1/5 * * * * *\"
+# sec   min   hour   day of month   month   day of week   year
+# *     *     *      *              *       *             *
+schedule = \"0 1/5 * * * * *\"
 
-  # The domain managed by DigitalOcean DNS
-  domain = \"example.com\"
+# The domain managed by DigitalOcean DNS
+domain = \"example.com\"
 
-  # The A Record to apply dynamic DNS
-  # Example: \"home\" -> home.example.com
-  record_name = \"home\" 
+# The A Record to apply dynamic DNS
+# Example: \"home\" -> home.example.com
+record_name = \"home\" 
 
-  # Get auth token from https://cloud.digitalocean.com/account/api/tokens
-  do_token = \"AUTH_TOKEN\"" >> /etc/do-ddns/config.toml
+# Get auth token from https://cloud.digitalocean.com/account/api/tokens
+do_token = \"AUTH_TOKEN\"" >> /etc/do-ddns/config.toml
   chmod 700 /etc/do-ddns/config.toml
 fi
 if systemctl --all --type service | grep -q "do-ddns.service"; then    
@@ -30,7 +30,9 @@ Type=simple
 Restart=always
 RestartSec=1
 User=root
+Group=root
 ExecStart=/usr/local/bin/do-ddns
+ExecReload=/bin/kill -HUP \$MAINPID
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/do-ddns.service
